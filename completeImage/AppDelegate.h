e44e15af9b288a85a655fb802f5a70baeb4e69a9
sheepcao <sheepcao1986@163.com>
2014-10-27 15:58:25 +0800

    ad added,next to delete 3,4,5 level and add an alert and  fix iphone 6 photo bug

---
+++ completeImage/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  completeImage
+//
+//  Created by 张力 on 14-6-4.
+//  Copyright (c) 2014年 张力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "globalVar.h"
+#import "GADInterstitial.h"
+
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate,GADInterstitialDelegate>
+{
+    GADInterstitial *splashInterstitial_;
+}
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property(nonatomic, readonly) NSString *interstitialAdUnitID;
+
+- (GADRequest *)createRequest;
+@end
---
+++ completeImage/globalVar.h
@@ -0,0 +1,46 @@
+//
+//  globalVar.h
+//  completeImage
+//
+//  Created by 张力 on 14-6-25.
+//  Copyright (c) 2014年 张力. All rights reserved.
+//
+
+#ifndef completeImage_globalVar_h
+#define completeImage_globalVar_h
+
+#define IPhoneHeight (([UIScreen mainScreen].bounds.size.height == 568.0)?568:460)
+#define TIME_PARENT 120
+
+#define bigLevel 5
+
+
+#import <iAd/iAd.h>
+#import "GADBannerView.h"
+#define ADMOB_ID @"ca-app-pub-3074684817942615/4079653086"
+#define ADMOB_ID_DaysInLine @"ca-app-pub-3074684817942615/1126186689"
+#import <ShareSDK/ShareSDK.h>
+#import "CommonUtility.h"
+#import "MobClick.h"
+
+int level ;
+NSNumber *levelSaved;
+int levelTop;
+NSMutableArray *haveShared;
+NSMutableArray *scores;
+NSString *haveSharedString;
+int seconds;
+BOOL isAnimating;
+
+NSArray *sharePic;
+NSArray *sharePic480;
+
+
+@protocol  backToLevelDelegate<NSObject>
+
+-(BOOL) isFromReward :(BOOL)check;
+@end
+
+
+
+#endif
